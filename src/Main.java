6890baf3e9c95ce6c5eb571826d4da469d69dff9
Rida GHOUTI TERKI <rida.ght@gmail.com>
2019-12-16 19:08:39 +0100

    Ajout de javadoc dans le Main

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,41 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        ARN<String> a;
+        /**
+         * Arbre affiché
+         */
+        ARN<String> arbre;
+        /**
+         * Ce qui sera écrit par l'utilisateur
+         */
         String input;
-        boolean b = true;
+        /**
+         * Condition pour que le programme tourne
+         */
+        boolean run = true;
+        /**
+         * Le mode de saisie (ajouter par défaut, si false = supprimer)
+         */
         boolean ajouter = true;
+        /**
+         * Scanner pour stocker la saisie de l'utilisateur dans "input"
+         */
         Scanner sc = new Scanner(System.in);
-        a = new ARN<>();
-        while(b) {
-            System.out.println(a);
+        arbre = new ARN<>();
+        while(run) {
+            System.out.println(arbre);
             if (ajouter) {
                 System.out.println("**** AJOUTER UN ÉLÉMENT : (tapez ':s' pour passer vers le mode 'suppression' || ':q' pour quitter le programme) ****");
                 input = sc.nextLine();
                 if (input.equals(":s")) ajouter = false;
                 else if (input.equals(":q")) return;
-                else a.add(input);
+                else arbre.add(input);
             } else {
                 System.out.println("**** SUPPRIMER UN ÉLÉMENT: (tapez ':a' pour passer vers le mode 'ajout' || ':q' pour quitter le programme) ****");
                 input = sc.nextLine();
                 if (input.equals(":a")) ajouter = true;
                 else if (input.equals(":q")) return;
-                else a.remove(input);
+                else arbre.remove(input);
             }
         }
     }
